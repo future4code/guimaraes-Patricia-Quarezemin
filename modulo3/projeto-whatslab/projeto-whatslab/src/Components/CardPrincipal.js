3aad954f198532c1707fcea41c42c025af9a15f8
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-02-06 22:17:59 -0300

    background e estado

--- modulo3/projeto-whatslab/projeto-whatslab/src/Components/CardPrincipal.js
+++ modulo3/projeto-whatslab/projeto-whatslab/src/Components/CardPrincipal.js
@@ -1,47 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import ImagemBackground from './Img/whatsFundo.jpeg'
 
 const InputRemetente = styled.input`
     width: 20%;
-    /* position: absolute;
-    bottom: 0;
-    margin: left;
-    justify-content: space-evenly; */
 `
 const InputMensagem = styled.input`
   width: 60%;
-  /* position: absolute;
-  bottom: 0;
-  margin:center;
-  justify-content: space-evenly; */
 `
 const ButtomEnviar = styled.button`
   width: 15%;
-  /* align-items: center;
-  position: absolute;
-  bottom: 0;
-  margin: right;
-  justify-content: space-evenly; */
 `
-
 const RetanguloMensagem = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   border-style: solid 5px;
   height: 80vh;
-  width: 50vw;
-  background-color: #ECE5DD;  
-`
+  width: 51vw;
+  background-image: url(${ImagemBackground});
+  background-size: cover;
+  `
 
 const BoxPrincipal = styled.div`
-  /* display: flex; */
-  /* grid-template-rows: 100wh; */
   justify-content: center;
   border-style: solid;
   width: 51vw;
-  /* grid-gap: 0px 0px; */
-  
 `
 const AlinharTudo = styled.div`
   display: flex;
@@ -71,14 +55,15 @@ class CardPrincipal extends React.Component {
   }
   render (){
     return (
+
       <AlinharTudo>
         <BoxPrincipal>
           <RetanguloMensagem>
             {this.state.remetenteWhatslab}
             {this.state.mensagemWhatslab}
           </RetanguloMensagem>
-          <InputRemetente placeholder={'Remetente'} onChange={this.onChangeRemetente} value={this.state.remetenteWhatslab}></InputRemetente>
-          <InputMensagem placeholder={'Mensagem'} onChange={this.onChangeMensagem} value={this.state.mensagemWhatslab}></InputMensagem>
+          <InputRemetente type='text' placeholder={'Remetente'} onChange={this.onChangeRemetente} value={this.state.remetenteWhatslab}></InputRemetente>
+          <InputMensagem type='text' placeholder={'Mensagem'} onChange={this.onChangeMensagem} value={this.state.mensagemWhatslab}></InputMensagem>
           <ButtomEnviar onClick={this.onClickEnviar}>Enviar</ButtomEnviar>
         </BoxPrincipal>
       </AlinharTudo>
