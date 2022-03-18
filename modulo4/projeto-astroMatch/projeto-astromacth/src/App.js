8a888857faa3a8f0bdaa7cab6635d46cce80abf0
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-03-18 19:04:48 -0300

    Separei os componentes

---
+++ modulo4/projeto-astroMatch/projeto-astromacth/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '././Components/Header/Header.jsx';
+import Main from '././Components/Main/Main.jsx';
+import Footer from '././Components/Footer/Footer.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
