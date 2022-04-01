d7d3478b6b4f31f0ad7d3d853bb5e65d532726f2
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-03-31 21:00:07 -0300

    Criadas páginas e o Coordinator

--- modulo4/projeto-labex/projeto-labex/src/Routes/Router.js
+++ modulo4/projeto-labex/projeto-labex/src/Routes/Router.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from '../Pages/HomePage'
-import ListTripsPage from '../Pages/ListTripsPage'
-import ApplicationFormPage from '../Pages/ApplicationFormPage'
-import LoginPage from '../Pages/LoginPage'
-import AdminHomePage from '../Pages/AdminHomePage'
-import TripDetailsPage from '../Pages/TripDetailsPage'
-import CreateTripPage from '../Pages/CreateTripPage'
+import HomePage from '../Pages/HomePage/HomePage'
+import ListTripsPage from '../Pages/ListTripsPage/ListTripsPage'
+import ApplicationFormPage from '../Pages/FormPage/ApplicationFormPage'
+import LoginPage from '../Pages/LoginPage/LoginPage'
+import AdminHomePage from '../Pages/AdmHomePage/AdminHomePage'
+import TripDetailsPage from '../Pages/TripDetailsPage/TripDetailsPage'
+import CreateTripPage from '../Pages/CreateTripPage/CreateTripPage'
 
 const Router = () => {
     return (
