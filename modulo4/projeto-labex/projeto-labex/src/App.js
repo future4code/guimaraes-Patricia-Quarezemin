0d5e62c1d10a8c64918ae3e2fa594c9d7cb62aad
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-04-01 16:11:07 -0300

    Coloquei uma imagem de fundo

--- modulo4/projeto-labex/projeto-labex/src/App.js
+++ modulo4/projeto-labex/projeto-labex/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import Router from './Routes/Router';
+import Router from './Routes/Router'
+import styled from '../src/styled.css'
 
 const App = () => {
   return (
-    <Router />
+  
+      <Router />
+  
   );
 }
 
