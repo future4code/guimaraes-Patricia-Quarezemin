520bf1be763f57cd8f94d0d1c90438952edb8de5
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-04-27 19:46:49 -0300

    Decidindo o tema do projeto - material-ui

--- modulo5/projeto-starwars/star-wars-app/src/constants/theme.js
+++ modulo5/projeto-starwars/star-wars-app/src/constants/theme.js
@@ -1,16 +1,63 @@
-// import { createTheme } from '@material-ui/core';
-// import { primaryColor, neutralColor } from './colors';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import { amber, deepOrange, grey } from '@mui/material/colors';
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: primaryColor,
-//       contrastText: '#ffff00',
-//     },
-//     secundary: {
-//       main: neutralColor
-//     }
-//   }
-// });
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === 'dark' && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === 'dark' && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === 'light'
+        ? {
+            primary: grey[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: '#fff',
+            secondary: grey[500],
+          }),
+    },
+  },
+});
 
-// export default theme
+function MyApp() {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+        p: 3,
+      }}
+    >
+      This is a {theme.palette.mode} mode theme with custom palette
+    </Box>
+  );
+}
+
+const darkModeTheme = createTheme(getDesignTokens('dark'));
+
+export default function DarkThemeWithCustomPalette() {
+  return (
+    <ThemeProvider theme={darkModeTheme}>
+      <MyApp />
+    </ThemeProvider>
+  );
+}
