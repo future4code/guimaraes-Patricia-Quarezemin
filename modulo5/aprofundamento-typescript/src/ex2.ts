91b468b4e5101cfb9e033aa517bb191ec36c2708
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-05-25 22:36:15 -0300

    Resolvendo os exercícios da aula de Aprofundamento-Typescript

---
+++ modulo5/aprofundamento-typescript/src/ex2.ts
@@ -0,0 +1,45 @@
+//Observe a função a seguir escrita em Javascript:
+
+function obterEstatisticas(numeros) {
+
+    const numerosOrdenados = numeros.sort(
+        (a:number, b:number) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+//a) Quais são as entradas e saídas dessa função? Copie a função para um arquivo .ts e faça a tipagem desses parâmetros.
+
+//b) Quais outras variáveis compõem essa função? Explicite a tipagem de todas elas.
+
+//c) Crie um type chamado amostra de dados, isto é, um objeto com as propriedades numeros e obterEstatisticas. Abeixo, 
+//temos um exemplo de objeto desse tipo, declarado em Javascript:
+
+// const amostraDeIdades = {
+
+//     numeros: [21, 18, 65, 44, 15, 18],
+
+//     obterEstatisticas: (numeros) => {...}
+// }
+
+type amostraDeDados = {
+    numeros: number,
+    obterEstatisticas: number
+}
+
+const verAmostras: amostraDeDados = {
+    numeros: 
+}
---
+++ modulo5/aprofundamento-typescript/src/ex3.ts
@@ -0,0 +1,52 @@
+//Considere que você esteja implementando uma rede social composta por posts de usuários. Cada um dos posts possui:
+//um autor e um texto. Observe o seguinte array de posts:
+
+const posts = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+//a) crie um type para representar o post. 
+
+type postRedeSocial = {
+    autor: string,
+    texto: string
+}
+
+//Utilize esse mesmo tipo criado acima para fazer a tipagem do array post:
+
+const Dumbledore: postRedeSocial = {
+    autor: 'Alvo Dumbledore',
+    texto: 'Não vale a pena viver sonhando e se esquecer de viver'
+}
+
+//b) Observe abaixo a função buscarPostsPorAutor(), escrita em Javascript:
+
+function buscarPostsPorAutor(posts: string, autorInformado: string) {
+    return posts.filter(
+      (post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+
+// Quais são as entradas e saídas dessa função? Copie a função para 
+//o mesmo arquivo .ts do array de posts e faça a tipagem necessária.
