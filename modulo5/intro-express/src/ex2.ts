2747988e6009ef430f7fd62f979e014b49816439
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-05-31 16:02:40 -0300

    Exercícios de API's e Express

---
+++ modulo5/intro-express/src/ex2.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import { Request, Response } from 'express'
+
+const app = express() //agora temos todas as funcionalidades do express
+
+app.use(express.json()) //através desta expressão, vamos trocar informações por meio do formato json
+app.use(cors()) //habilita o cors
+
+app.get('/users', (req: Request, res: Response)=>{
+    res.status(200).send('Usuários visualizados')
+})
+
+app.listen(3003, ()=>{
+    console.log('Servidor de pé')
+})
+
+
+
+type user = {
+    id: number,
+    name: string,
+    phone: number,
+    email: string,
+    website: string | number
+}
