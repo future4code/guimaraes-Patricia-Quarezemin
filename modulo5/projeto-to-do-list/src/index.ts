f52de2ca90062e3c9be0d31090d4ec040fb19edf
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-06-30 16:04:37 -0300

    Projeto ToDoList

--- modulo5/projeto-to-do-list/src/index.ts
+++ modulo5/projeto-to-do-list/src/index.ts
@@ -1,16 +1,5 @@
-import connection from './connection'
 import app from './app'
-import { Request, Response } from 'express'
 
-app.post('/user', async (req: Request, res: Response)=>{
+app.post('/user',)
 
-    const { name, nickname, email } = req.body;
 
-    try{
-        await connection('')
-
-    } catch(e) {
-        console.error({e});
-        return res.status(500).send('Algo deu errado!')
-    }
-})
---
+++ modulo5/projeto-to-do-list/types.ts
@@ -0,0 +1,5 @@
+export type user = {
+    name: string,
+    nickname: string,
+    email: string
+}
