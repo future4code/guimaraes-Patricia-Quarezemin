bebacfba119ac613963d624c6ee3f971bb26cc78
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-07-11 20:52:45 -0300

    Classes-encapsulamento - exercícios

---
+++ modulo5/classes-encapsulamento/src/data/Transaction.ts
@@ -0,0 +1,17 @@
+// type Transaction = {
+//     description: string,
+//     value: number,
+//     date: string
+//   }
+
+export class Transaction {
+    private description: string
+    private value: number
+    private date: string
+}
+
+ constructor (description: string, value: number, date: string) {
+    this.description = description,
+    this.value = value,
+    this.date = date
+}
