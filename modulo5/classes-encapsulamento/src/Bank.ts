9e402a91a13135a726412a21cb83a8473473803f
Paty-quarezemin <patriciargquarezemin@gmail.com>
2022-07-12 20:59:49 -0300

    Exercício - Classe e encapsulamento

--- modulo5/classes-encapsulamento/src/Bank.ts
+++
@@ -1,9 +0,0 @@
-import { UserAccount } from './UserAccount'
-
-// export class Bank {
-//     private accounts: UserAccount[]
-
-//     constructor(accounts: UserAccount[] {
-//         this.accounts = accounts
-//     })
-// }
